9bb68f40d843f445bd5fccc1be3bfee8f8a13dce
Khizar Wakeel <khizarwakeel@gmail.com>
2024-02-02 23:45:18 +0500

    added whalefundr Website and added header

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,212 @@
+"use client";
+
+import { Fragment, useState } from "react";
+import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
+import Logo from "/public/assets/logo.png";
+import ukLogo from "/public/assets/ukLogo.png";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import Image from "next/image";
+import CountryList from "../resuseComp/countryList";
+
+function classNames(...classes: string[]): string {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Header() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <header className="bg-gradient-to-r from-[#121212] to-[#142532]">
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <Image src={Logo} alt="Whale" className="xl:w-56 w-44" />
+          </a>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <Popover.Group className="hidden lg:flex xl:gap-x-8 lg:gap-x-5 text-white">
+          <Popover className="relative">
+            <Popover.Button className="flex items-center gap-x-1 outline-none text-sm font-semibold leading-6">
+              How it works
+              <ChevronDownIcon
+                className="h-5 w-5 flex-none"
+                aria-hidden="true"
+              />
+            </Popover.Button>
+
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute bg-gray-800 text-white pt-5 text-center -left-8 top-full z-10 mt-3 max-w-56 w-screen overflow-hidden bg-inherit shadow-lg ring-1 ring-gray-900/5">
+                <ul className="">
+                  <li className="mb-5">1</li>
+                  <li className="mb-5">2</li>
+                  <li className="mb-5">3</li>
+                  <li className="mb-5">4</li>
+                </ul>
+              </Popover.Panel>
+            </Transition>
+          </Popover>
+          <a href="#" className="text-sm font-semibold leading-6">
+            FAQ
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6">
+            Testimonials
+          </a>
+          <Popover className="relative">
+            <Popover.Button className="flex items-center gap-x-1 outline-none text-sm font-semibold leading-6">
+              Trading
+              <ChevronDownIcon
+                className="h-5 w-5 flex-none"
+                aria-hidden="true"
+              />
+            </Popover.Button>
+
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute bg-gray-800 text-white pt-5 text-center -left-8 top-full z-10 mt-3 max-w-56 w-screen overflow-hidden bg-inherit shadow-lg ring-1 ring-gray-900/5">
+                <ul className="">
+                  <li className="mb-5">1</li>
+                  <li className="mb-5">2</li>
+                  <li className="mb-5">3</li>
+                  <li className="mb-5">4</li>
+                </ul>
+              </Popover.Panel>
+            </Transition>
+          </Popover>
+          <a href="#" className="text-sm font-semibold leading-6">
+            About Us
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6">
+            Contact Us
+          </a>
+        </Popover.Group>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end gap-6 items-center">
+          <button className="bg-[#2176B9] w-12 h-12 rounded-full flex justify-center items-center">
+            <svg
+              width="15"
+              height="15"
+              viewBox="0 0 20 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M14.2939 12.5786H13.3905L13.0703 12.2699C14.2297 10.9251 14.8669 9.20834 14.8656 7.43282C14.8656 5.96275 14.4297 4.52569 13.613 3.30337C12.7963 2.08105 11.6354 1.12837 10.2772 0.565793C8.91907 0.00322052 7.42457 -0.143974 5.98275 0.142823C4.54092 0.42962 3.21652 1.13753 2.17702 2.17702C1.13753 3.21652 0.42962 4.54092 0.142823 5.98275C-0.143974 7.42457 0.00322052 8.91907 0.565793 10.2772C1.12837 11.6354 2.08105 12.7963 3.30337 13.613C4.52569 14.4297 5.96275 14.8656 7.43282 14.8656C9.27387 14.8656 10.9663 14.191 12.2699 13.0703L12.5786 13.3905V14.2939L18.2962 20L20 18.2962L14.2939 12.5786ZM7.43282 12.5786C4.58548 12.5786 2.28702 10.2802 2.28702 7.43282C2.28702 4.58548 4.58548 2.28702 7.43282 2.28702C10.2802 2.28702 12.5786 4.58548 12.5786 7.43282C12.5786 10.2802 10.2802 12.5786 7.43282 12.5786Z"
+                fill="#F0F0F0"
+              />
+            </svg>
+          </button>
+          <div>
+            <CountryList country="Eng" image={ukLogo} />
+          </div>
+        </div>
+      </nav>
+      {/* Mobile Bar */}
+      <Dialog
+        as="div"
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+      >
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto text-white bg-gradient-to-b from-[#121212] to-[#142532] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <Image src={Logo} alt="Whale" className="w-44" />
+            </a>
+            <button
+              type="button"
+              className=" rounded-md"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                <Disclosure as="div" className="-mx-3">
+                  {({ open }) => (
+                    <>
+                      <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 hover:bg-gray-50">
+                        Product
+                        <ChevronDownIcon
+                          className={classNames(
+                            open ? "rotate-180" : "",
+                            "h-5 w-5 flex-none"
+                          )}
+                          aria-hidden="true"
+                        />
+                      </Disclosure.Button>
+                      <Disclosure.Panel className="mt-2 space-y-2">
+                        <Disclosure.Button
+                          as="a"
+                          className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 hover:bg-gray-50"
+                        >
+                          1
+                        </Disclosure.Button>
+                      </Disclosure.Panel>
+                    </>
+                  )}
+                </Disclosure>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-gray-50"
+                >
+                  Features
+                </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-gray-50"
+                >
+                  Marketplace
+                </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-gray-50"
+                >
+                  Company
+                </a>
+              </div>
+              <div className="py-6">
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 hover:bg-gray-50"
+                >
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </header>
+  );
+}
